5fcd103688441b9b49da91683ae7073944695b91
SRUTHI <sruthimolbiju21@gmail.com>
2021-11-07 10:19:13 +0530

    python

--- Tutorial3/InsuranceEmployeeNominee.py
+++ Tutorial3/InsuranceEmployeeNominee.py
@@ -7,41 +7,24 @@ class Employee:
         self.name=name
         self.salary=salary
         self.insurance=insurance
-
-
-class Personal:
+class Nominee:
     def _init_(self,status,nomininame,dob):
         self.status=status
         self.nomininame=nomininame
         self.dob=dob
-
-
-
-class Insurance(Employee,Personal):
+class Insurance(Employee,Nominee):
     def _init_(self,empid,name,salary,insurance,status,nomininame,dob):
         Employee._init_(self,empid,name,salary,insurance)
-        Personal._init_(self,status,nomininame,dob)
-
+        Nominee._init_(self,status,nomininame,dob)
     def display(self):
         today = date.today()
-        # print("Today's date:", today)
         age= today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
         sam=self.status
         if age >=18 and (sam ==False):
             amt=self.insurance*12*12
             print(self.nomininame,amt)
-
-
-dob = datetime.date(1982,8,7)
-# birthday = input("Enter your date of birth: ")
-
-# bday = datetime.strptime(birthday, '%d/%m/%Y')
+dob = datetime.date(1999,3,9)
 today = date.today()
-# print("Today's date:", today)
-
 age= today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
-# print(age)
-
-
-s1=Insurance(101,'Rahul',100000,2000,False,"Jibin",dob)
+s1=Insurance(2001,'Sruthi',300000,2000,False,"Bindhu",dob)
 s1.display()
